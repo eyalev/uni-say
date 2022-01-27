f0c0c68689dca505948dbe422aa9084c3e9f9771
Eyal Levin <eyalev@gmail.com>
2022-01-27 14:05:30 +0200

    Update

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
 
 # Get the long description from the README file
 long_description = (here / "README.md").read_text(encoding="utf-8")
